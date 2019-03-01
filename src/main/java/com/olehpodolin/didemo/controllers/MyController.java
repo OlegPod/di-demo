bc2d2c478c77c3277ccd2d58ee7c8110de2c16fa
OlegPod <kepacuh@gmail.com>
2019-03-02 00:15:40 +0200

    Using Primary Annotation

--- src/main/java/com/olehpodolin/didemo/controllers/MyController.java
+++ src/main/java/com/olehpodolin/didemo/controllers/MyController.java
@@ -1,14 +1,21 @@
 package com.olehpodolin.didemo.controllers;
 
+import com.olehpodolin.didemo.services.GreetingService;
 import org.springframework.stereotype.Controller;
 
 
 @Controller
 public class MyController {
 
+    GreetingService greetingService;
+
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
     public String hello(){
         System.out.println("Hello!!! ");
 
-        return "foo";
+        return greetingService.sayGreeting();
     }
 }

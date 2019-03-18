0279b45f9d9b70e49926bb26b02cb5ba42e9db6a
OlegPod <kepacuh@gmail.com>
2019-03-18 17:43:43 +0200

    Using Spring Factory Beans

--- src/main/java/com/olehpodolin/didemo/services/PrimaryGermanGreetingService.java
+++ src/main/java/com/olehpodolin/didemo/services/PrimaryGermanGreetingService.java
@@ -4,10 +4,15 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Service
-@Primary
-@Profile("de")
+
 public class PrimaryGermanGreetingService implements GreetingService {
+
+    private GreetingRepository greetingRepository;
+
+    public PrimaryGermanGreetingService(GreetingRepository greetingRepository) {
+        this.greetingRepository = greetingRepository;
+    }
+
     @Override
     public String sayGreeting() {
         return "Primärer Grußdienst";

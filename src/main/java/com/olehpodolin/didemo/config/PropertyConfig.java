79c9560820f9d158842c1d0ae3766bcf2919cb77
OlegPod <kepacuh@gmail.com>
2019-03-21 23:27:13 +0200

    External Property Source

---
+++ src/main/java/com/olehpodolin/didemo/config/PropertyConfig.java
@@ -0,0 +1,38 @@
+package com.olehpodolin.didemo.config;
+
+import com.olehpodolin.didemo.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration
+@PropertySource("classpath:datasource.properties")
+public class PropertyConfig {
+
+    @Value("${olehpodolin.user}")
+    private String user;
+
+    @Value("${olehpodolin.password}")
+    private String password;
+
+    @Value("${olehpodolin.dburl}")
+    private String url;
+
+    @Bean
+    public FakeDataSource fakeDataSource() {
+        FakeDataSource fakeDataSource = new FakeDataSource();
+        fakeDataSource.setUser(user);
+        fakeDataSource.setPassword(password);
+        fakeDataSource.setUrl(url);
+        return fakeDataSource;
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer properties() {
+        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer =
+                new PropertySourcesPlaceholderConfigurer();
+        return propertySourcesPlaceholderConfigurer;
+    }
+}
